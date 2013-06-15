677413b192c01d1560004d48baa1a431eb3422f1
kputnam <235798+kputnam@users.noreply.github.com>
2013-06-15 15:36:22 -0500

    Convert regular expressions to NFA

--- src/Language/Grammar/Regular.hs
+++ src/Language/Grammar/Regular.hs
@@ -2,6 +2,8 @@ module Language.Grammar.Regular where
 
 import qualified Language.Automata.Finite.Nondeterministic as N
 
+import Control.Monad.State
+
 data Regexp a
   = Empty
   | Literal a
@@ -13,9 +15,40 @@ data Regexp a
 -- parse  :: Parser Regexp Char
 -- pretty :: Regexp Char -> String
 
-toNFA :: Regexp a -> N.NFA Int a
-toNFA Empty        = undefined
-toNFA (Literal _)  = undefined
-toNFA (Concat _ _) = undefined
-toNFA (Choose _ _) = undefined
-toNFA (Repeat _)   = undefined
+inc :: (Enum s, MonadState s m) => m s
+inc = do
+  n <- get
+  modify succ
+  return n
+
+toNFA :: (Enum s, Ord s, Ord a) => Regexp a -> s -> N.NFA s a
+toNFA r = uncurry N.build . evalState (build r)
+  where
+    build Empty        = do
+      n <- inc
+      return ([], [n])
+
+    build (Literal a)  = do
+      n <- inc
+      m <- inc
+      return ([(n, N.Token a, m)], [m])
+
+    build (Concat a b) = do
+      (ta, aa) <- build a; sb <- get
+      (tb, ab) <- build b
+      let ts = map (\x -> (x, N.Epsilon, sb)) aa
+      return (ta ++ tb ++ ts, ab)
+
+    build (Choose a b) = do
+      ss <- inc
+      sa <- get; (ta, aa) <- build a
+      sb <- get; (tb, ab) <- build b
+      let ts = [(ss, N.Epsilon, sa), (ss, N.Epsilon, sb)]
+      return (ta ++ tb ++ ts, aa ++ ab)
+
+    build (Repeat a)   = do
+      ss <- inc
+      sa <- get; (ta, aa) <- build a
+      let ts = map (\x -> (x, N.Epsilon, sa)) aa
+          tt = (ss, N.Epsilon, sa)
+      return (tt:ta ++ ts, ss:aa)
