4b7fe5756b8a81257172114bf62302c0e280dfdd
kputnam <235798+kputnam@users.noreply.github.com>
2013-06-17 01:44:15 -0500

    Define toList, fromList for NFA & DFA. Begin NFA to DFA conversion

--- src/Language/Automata/Finite/Deterministic.hs
+++ src/Language/Automata/Finite/Deterministic.hs
@@ -1,14 +1,16 @@
 module Language.Automata.Finite.Deterministic
   ( DFA
-  , build
+  , fromList
+  , toList
   , eval
   ) where
 
 import Prelude hiding (lookup)
 import Data.Maybe (fromMaybe)
 import Data.List (foldl')
-import Data.Map (Map, fromListWith, fromList, union,
-                 singleton, lookup, empty, unionWith)
+import Data.Map (Map, fromListWith, union, singleton,
+                 elems, lookup, empty, unionWith)
+import qualified Data.Map as M
 
 data State s t
   = Stuck
@@ -19,14 +21,22 @@ newtype DFA s t
   = DFA { getM :: Map s (State s t) }
   deriving (Eq, Show, Read)
 
-build :: (Ord s, Ord t) => [(s, t, s)] -> [s] -> DFA s t
-build trans accept = DFA (unionWith combine incoming outgoing)
+fromList :: (Ord s, Ord t) => [(s, t, s)] -> [s] -> DFA s t
+fromList edges accept = DFA (unionWith combine incoming outgoing)
   where
     combine (State s b m) (State _ _ n) = State s b (m `union` n)
-    incoming = fromList (map fromAccept accept)
-    outgoing = fromListWith combine (map fromTrans trans)
+    incoming = M.fromList (map fromAccept accept)
+    outgoing = fromListWith combine (map fromEdges edges)
     fromAccept s        = (s, State s True empty)
-    fromTrans (a, t, b) = (a, State a (a `elem` accept) (singleton t b))
+    fromEdges (a, t, b) = (a, State a (a `elem` accept) (singleton t b))
+
+toList :: DFA s t -> ([(s, t, s)], [s])
+toList m = (edges =<< states, accept states)
+  where
+    states              = elems (getM m)
+    edges (State a _ t) = map (brand a) (M.toList t)
+    brand a (t, b)      = (a, t, b)
+    accept xs           = [ a | State a b _ <- xs, b ]
 
 step :: (Ord s, Ord t) => DFA s t -> State s t -> t -> State s t
 step _ Stuck _          = Stuck
--- src/Language/Automata/Finite/Nondeterministic.hs
+++ src/Language/Automata/Finite/Nondeterministic.hs
@@ -1,16 +1,19 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Language.Automata.Finite.Nondeterministic
   ( NFA
   , Token(..)
   , State(State)
-  , build
+  , fromList
+  , toList
   , eval
   , toDFA
   ) where
 
 import Prelude hiding (lookup, read)
 import Data.Maybe (fromMaybe)
-import Data.Map (Map, lookup, fromListWith, unionWith)
-import Data.Set (Set, empty, insert, fold, union, toList)
+import Data.Map (Map, lookup, fromListWith, unionWith, elems)
+import Data.Set (Set, empty, insert, fold, union, member)
 import Data.List (foldl')
 import qualified Data.Map as M
 import qualified Data.Set as S
@@ -30,14 +33,22 @@ newtype NFA s t
   = NFA { getM :: Map s (State s t) }
   deriving (Eq, Show, Read)
 
-build :: (Ord s, Ord t) => [(s, Token t, s)] -> [s] -> NFA s t
-build trans accept = NFA (unionWith combine incoming outgoing)
+fromList :: (Ord s, Ord t) => [(s, Token t, s)] -> [s] -> NFA s t
+fromList edges accept = NFA (unionWith combine incoming outgoing)
   where
     combine (State s b m) (State _ _ n) = State s b (unionWith union m n)
     incoming = M.fromList (map fromAccept accept)
-    outgoing = fromListWith combine (map fromTrans trans)
+    outgoing = fromListWith combine (map fromEdges edges)
     fromAccept s        = (s, State s True M.empty)
-    fromTrans (a, t, b) = (a, State a (a `elem` accept) (M.singleton t (S.singleton b)))
+    fromEdges (a, t, b) = (a, State a (a `elem` accept) (M.singleton t (S.singleton b)))
+
+toList :: NFA s t -> ([(s, Token t, s)], [s])
+toList m = (edges =<< states, accept states)
+  where
+    states              = elems (getM m)
+    edges (State a _ t) = brand a =<< M.toList t
+    brand a (t, bs)     = map (\b -> (a, t, b)) (S.toList bs)
+    accept xs           = [ a | State a b _ <- xs, b ]
 
 read :: (Ord s, Ord t) => NFA s t -> State s t -> Token t -> Set (State s t)
 read _ Stuck _          = empty
@@ -46,6 +57,7 @@ read m (State _ _ ts) t = case lookup t ts of
   Just ss -> fold f empty ss
   where f s r = maybe r (`insert` r) (lookup s (getM m))
 
+-- TODO: Detect loops by passing original start state to recursive calls
 free :: (Ord s, Ord t) => NFA s t -> State s t -> Set (State s t)
 free m s = s `insert` fold union ss (S.map (free m) ss)
   where ss = read m s Epsilon
@@ -54,12 +66,48 @@ step :: (Ord s, Ord t) => NFA s t -> State s t -> t -> Set (State s t)
 step m s t = fold (union . free m) empty (read m s (Token t))
 
 eval :: (Ord s, Ord t) => NFA s t -> s -> [t] -> Bool
-eval m i = any accept . toList . foldl' step' (start `insert` free m start)
+eval m i = any accept . S.toList . foldl' step' start
   where
-    start      = fromMaybe Stuck (lookup i (getM m))
+    start      = free m (fromMaybe Stuck (lookup i (getM m)))
     step' ss t = fold (\s -> union (step m s t)) empty ss
     accept Stuck         = False
     accept (State _ x _) = x
 
-toDFA :: NFA s t -> D.DFA s t
-toDFA = undefined
+toDFA :: forall s t. (Ord s, Ord t) => NFA s t -> s -> D.DFA (Set s) t
+toDFA m i = undefined --uncurry D.fromList (loop S.empty start)
+  where
+    start   = free m (state i)
+    state s = fromMaybe (dummy s) (lookup s (getM m))
+    dummy s = State s False M.empty
+    label   = S.map (\(State s _ _) -> s)
+
+    -- TODO
+    loop :: Set (Set s) -> [State (Set s) t] -> ([(Set s, t, Set s)], [Set s])
+    loop = loop' ([], [])
+      where
+        loop' acc done [] = acc
+        loop' acc done (s@(State a b t):xs)
+          | a `member` done = loop' acc done xs
+          | otherwise       = tuple s
+
+    tuple :: State (Set s) t -> ([(Set s, t, Set s)], [Set s])
+    tuple (State s b t) = (trans, accept)
+      where
+        trans  = map (\(Token t', b) -> (s, t', join b)) (M.toList t)
+        accept = [ s | b ]
+        join   = S.unions . S.toList
+
+    -- State 1 T (a -> {1,2})
+    -- State 2 F (a -> {2,3})
+    -- ---------------------------------
+    -- State {1,2} T (a -> {{1,2},{2,3}}
+    flatten :: Set (State s t) -> State (Set s) t
+    flatten ss = State (label freed) (accept freed) (edges freed)
+      where
+        freed     = fold (union . free m) empty ss
+        accept    = S.fold (\(State _ a _) -> (|| a)) False
+        edges     = S.fold (M.unionWith merge . tx) M.empty
+        merge a b = S.singleton (S.unions (S.toList a ++ S.toList b))
+        tx s@(State _ _ t) = M.fromList (fx s =<< M.toList t)
+        fx _ (Epsilon, _)  = []
+        fx s (Token t, _)  = [(Token t, S.singleton (label (step m s t)))]
